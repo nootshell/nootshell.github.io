33365d58003c4f2d3b054fb9a0c23affdf49c30d
God <strawberryentrypoint@gmail.com>
2017-02-15 09:39:29 +0100

    Renamed 'style' to 'css'.

--- js/lib/shared.js
+++ js/lib/shared.js
@@ -7,7 +7,7 @@ function add_script(src) {
 }
 
 function set_theme_dark(enabled,setcookie) {
-	window.theme_link.setAttribute('href', './style/override.' + (enabled ? 'dark' : 'none') + '.css');
+	window.theme_link.setAttribute('href', './css/override.' + (enabled ? 'dark' : 'none') + '.css');
 	if (setcookie) {
 		Cookies.set('theme_dark', (enabled ? 'y' : 'n'), { expires: 64 });
 	}
